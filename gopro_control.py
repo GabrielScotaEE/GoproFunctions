cacad85c27ac214256f1211261a865ae0889be8a
gabriel <scota07@gmail.com>
2021-06-15 15:58:54 -0300

    added video play

--- gopro_control.py
+++ gopro_control.py
@@ -51,38 +51,59 @@ class GP_functions:
             cv2.waitKey(0)
     
     def show_some_downloadeds_videos(self):
-        cv2.namedWindow('sample video', 0)
-        for i in range(len(gopro_functions.videos_list)):
-            #os.listdir('.\images')[i]
-            vd = cv2.VideoCapture('.\images\GH01095.MP4')
-            while (vd.isOpened()):
-                ret, frame = vd.read()
-                if ret == True:
-                    cv2.imshow('sample image',frame)
-                    if cv2.waitKey(25) & 0xFF == ord('q'):
-                        break
-        vd.release()
+        # Create a VideoCapture object and read from input file
+        video_name_file = os.listdir('.\images')[0]
+        
+        cap = cv2.VideoCapture('.\images\{}'.format(video_name_file))
+
+        # Check if camera opened successfully
+        if (cap.isOpened()== False):
+            print("Error opening video file")
+
+            # Read until video is completed
+        while(cap.isOpened()):
+                
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if ret == True:
+
+            # Display the resulting frame
+                cv2.imshow('Frame', frame)
+
+            # Press Q on keyboard to exit
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+
+
+        # When everything done, release
+        # the video capture object
+        cap.release()
+
+        # Closes all the frames
+        cv2.destroyAllWindows()
                     
        
          
 
 
-
-
-
 gopro_functions = GP_functions()
 
+#goproCamera.video_settings(res="1080",fps="30")
 #goproCamera.shoot_video(duration=5)
 #gopro_functions.media_download_and_change_directory()
-gopro_functions.load_img_from_folder('.\images\*.JPG')
-print(gopro_functions.images_list)
-gopro_functions.load_videos_from_folder('.\images\*.MP4')
-print(gopro_functions.videos_list)
+#goproCamera.delete("all")
+#gopro_functions.load_img_from_folder('.\images\*.JPG')
+#print(gopro_functions.images_list)
+#gopro_functions.load_videos_from_folder('.\images\*.MP4')
+#print(gopro_functions.videos_list)
 
 
-gopro_functions.show_some_downloadeds_imgs()
+#gopro_functions.show_some_downloadeds_imgs()
+gopro_functions.show_some_downloadeds_videos()
 
-        
 
 
 
