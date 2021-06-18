e4115165ee38b4fc41028365e4495d9b17399f93
gabriel <scota07@gmail.com>
2021-06-18 16:27:55 -0300

    Added qr_decode.py

--- gopro_control.py
+++ gopro_control.py
@@ -7,6 +7,8 @@ import os
 from skimage.io import imread_collection
 import numpy as np
 from pyzbar.pyzbar import decode
+from qr_decode import qr_decoder
+import time
 
 
 # Creating obj gopro
@@ -85,77 +87,15 @@ class GP_functions:
         # Closes all the frames
         cv2.destroyAllWindows()
 
-    def try_to_decode_and_show_frame_if_is_decoded(self,path):
-            # set up camera object
-            cap = cv2.VideoCapture(path)
-
-            # Variable that says if has any decodification on the code
-            flag = 0
-
-            # Geting the total number of frames in video file
-            totalFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-            
-            for i in range(int(totalFrames)):
-                # get the frame image
-                sucess, frame = cap.read()
-            
-                if sucess == True:
-                    # Trying to decode any qrcode in frame
-                    msg = decode(frame)
-                    
-                    if flag >=1:
-                        break
-                    if(msg is not None):
-                        
-                        for code in msg:
-
-                            # Coordinates of bounding box in qrcode
-                            x, y, w, h = code.rect
-                            cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
-                            # Decoding de msg of qr code
-                            self.barcode_info = code.data.decode('utf-8')
-
-                            # Inserting text in image frame with the decoded msg
-                            font = cv2.FONT_HERSHEY_TRIPLEX
-                            cv2.rectangle(frame, (x, y),(x+395, y-50), (0, 255, 0), -1)
-                            cv2.putText(frame, self.barcode_info, (x + 6, y - 6), font, 2.0, (0, 0, 255), 2)
-                            
-                            #cv2.namedWindow('Barcode/QR code reader', 0)
-                            # Resizing the window shape
-                            width = int(frame.shape[1]*0.6)
-                            height = int(frame.shape[0]*0.6)
-                            dimensions = (width,height)
-                            frame_resized = cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
-
-                            #Showing the frame with the msg and the bounding box
-                            cv2.imshow('Barcode/QR code reader', frame_resized)
-
-                            # Creating .txt file with the msg of qrcode
-                            with open("barcode_result.txt", mode ='w') as file:
-                                file.write("Recognized Barcode:" + self.barcode_info)
-                            flag = flag+ 1
-                            if(cv2.waitKey(0)):
-                                break 
-           
-                    if(cv2.waitKey(1) == ord("q")):
-                        break
-            # Verify if there was no one decodification
-            if flag == 0:
-                print("Could'nt decode any qrcode in this file")
-
-            # free camera object and exit
-            cap.release()
-            cv2.destroyAllWindows()
-                    
-       
-         
-
 
 gopro_functions = GP_functions()
+decoder = qr_decoder
+begin = time.time()
+decoder.decode_and_show_all_videoFrames(decoder,'GH010302.MP4')
+end = time.time()
 
-gopro_functions.try_to_decode_and_show_frame_if_is_decoded('GH010308.MP4')
-
-
+print('A funçao demora: {} segundos'.format(end-begin))
+#decoder.decode_once_and_show_frame(decoder,'GH010302.MP4')
 #goproCamera.video_settings(res="1080",fps="30")
 #goproCamera.shoot_video(duration=5)
 #gopro_functions.media_download_and_change_directory()
