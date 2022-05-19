94bcb9db00733041b95f91fc3b610d646478d751
GabrielScotaEE <gabriel.scota@cotefacil.com>
2022-05-19 11:33:41 -0300

    remove unused imports | fix to pep8 convenction

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-  
-import cv2 as cv
-from pyzbar.pyzbar import decode
-import numpy as np
-from time import time
-from windowcapture import *
-from re import T
-from pyzbar.pyzbar import decode
-import socket
 from decoder import Decode
 from gopro_control import GoproClass
-
+from windowcapture import *
 
 # ffplay -fflags nobuffer -f:v mpegts -probesize 8192 udp://@10.5.5.9:8554
 # 10.5.5.9/gp/gpControl/execute?p1=gpStream&c1=restart
@@ -29,29 +20,28 @@ goprofunctions = GoproClass()
 
 # Loop through every screenshot creating a video from then
 
-while(True):
+while True:
     # Call function to keep gopro alive
-    goprofunctions.GoproHero8_keepAlive()
+    goprofunctions.gopro_hero8_keep_alive()
     # Getting the frame of the window with GoPro live.
 
-    #screenshot = wincap.get_sec_screen(1600,900,1368,768)
+    # screenshot = wincap.get_sec_screen(1600,900,1368,768)
 
     screenshot = wincap.get_screenshot()
     opencv_screenshot = np.array(screenshot)
     # Trying to decode any qrcode in th frame
-    decode_img, msg, only_qr = dcd.decode_opencv(opencv_screenshot)
+    decode_img, msg = dcd.decode_pyzbar(opencv_screenshot)
     if msg is not None:
         cv.imshow('Gopro Real Time', decode_img)
-        
-        cv.imshow('Only Qr', only_qr)
-        #print('A mensagem do qr code é: {}'.format(msg))
-        
+
+        # cv.imshow('Only Qr', only_qr)
+        # print('A mensagem do qr code é: {}'.format(msg))
+
     else:
         cv.imshow('Gopro Real Time', opencv_screenshot)
-        
 
     # Showing FPS live.
-    print('FPS {}'.format(np.round(1/(time()-loop_time))))
+    print('FPS {}'.format(np.round(1 / (time() - loop_time))))
 
     loop_time = time()
 
